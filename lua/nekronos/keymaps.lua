43bf0ea3f833c8a41a69986a0fe67f317e59d176
Eduardo Rodriguez <eduardo.rodriguez@ericsson.com>
2025-03-20 14:15:48 +0100

    NEW: keybindings for vertical terminal toggle + Window resizing
    Change-Id: I8759f1ae4e54f91b1f973cb247b99fac3d8f4190

--- lua/nekronos/keymaps.lua
+++ lua/nekronos/keymaps.lua
@@ -5,7 +5,7 @@ vim.api.nvim_set_keymap('n', '<leader>x', '<cmd>bp<bar>sp<bar>bn<bar>bd<cr>', {n
 -- FZF usage
 vim.keymap.set('n', '<leader>ff', '<cmd>FzfLua files<cr>') -- File search
 vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua live_grep<cr>') -- Grep search
-vim.keymap.set('n', '<leader>fv', '<cmd>FzfLua grep_visual<cr>') -- Grep search highlighted content under visual
+vim.keymap.set('v', '<leader>fv', '<cmd>FzfLua grep_visual<cr>') -- Grep search highlighted content under visual
 vim.keymap.set('n', '<leader>fb', '<cmd>FzfLua buffers<cr>') -- Grep search
 
 -- LSP Diagnostics
@@ -14,6 +14,8 @@ vim.keymap.set('n', '[g', vim.diagnostic.goto_prev)
 
 -- Toggle terminal
 vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>lua require("toggleterm").toggle(vim.v.count)<cr>', {noremap=true}) -- Toggle terminal by preceeding number
+vim.api.nvim_set_keymap('n', '<leader>T', '<cmd>lua require("toggleterm").toggle(vim.v.count, nil, nil, "vertical")<cr>', {noremap=true}) -- Vertical version
+
 vim.api.nvim_set_keymap('t', '<C-w>h', '<C-\\><C-n><C-w>h', {noremap=true, silent=true}) -- Use movement when on terminal
 vim.api.nvim_set_keymap('t', '<C-w>j', '<C-\\><C-n><C-w>j', {noremap=true, silent=true})
 vim.api.nvim_set_keymap('t', '<C-w>k', '<C-\\><C-n><C-w>k', {noremap=true, silent=true})
@@ -22,6 +24,11 @@ vim.api.nvim_set_keymap('t', '<C-w>l', '<C-\\><C-n><C-w>l', {noremap=true, silen
 -- Quick escape from terminal mode
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap=true, silent=true})
 
+-- Resize windows using Alt + arrow keys
+vim.api.nvim_set_keymap('n', '<M-k>', '<cmd>resize +5<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<M-j>', '<cmd>resize -5<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<M-h>', '<cmd>vertical resize -5<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<M-l>', '<cmd>vertical resize +5<CR>', {noremap=true})
 
 vim.api.nvim_create_user_command('Wqa', function()
   vim.cmd('wa')
