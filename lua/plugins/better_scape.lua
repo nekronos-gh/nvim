cca3032fd708c8950bef0064dd2b2aacc872b187
Eduardo Rodríguez Sánchez <edubuntu2002@gmail.com>
2026-03-11 21:53:45 +0100

    Fix escape on visual mode

--- lua/plugins/better_scape.lua
+++ lua/plugins/better_scape.lua
@@ -2,7 +2,7 @@ return {
   "max397574/better-escape.nvim",
   opts = {
     timeout = vim.o.timeoutlen,
-    default_mappings = true, -- setting this to false removes all the default mappings
+    default_mappings = false, -- setting this to false removes all the default mappings
     mappings = {
       -- i for insert
       i = {
